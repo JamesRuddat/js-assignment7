6091c8c2e4c3953c764acb656c5f328d30ce3012
James <300089902@fvtc.edu>
2024-04-14 17:01:22 -0500

    Assignment 7

--- app.js
+++ app.js
@@ -17,15 +17,36 @@ app.get('/', (_, response) => {
 	response.sendFile('index.html', { root })
 })
 
-
-
 // GET /api/todos
+app.get('/api/todos', (_, response) => {
+	response.json(todos)
+})
 
 // POST /api/todos
+app.post('/api/todos', (request, response) => {
+	const { item } = request.body
+	const newTodo = {
+		id: todos.length + 1,
+		item,
+		complete: false
+	}
+	todos.push(newTodo)
+	response.status(201).json(newTodo)
+})
 
 // PUT /api/todos/:id
+app.put('/api/todos/:id', (request, response) => {
+	const { id } = request.params
+	const task = todos.find(todo => todo.id.toString() === id)
 
-
+	if (task) {
+		task.complete = !task.complete
+		response.json(task)
+	}
+	else {
+		response.status(404).json({ message: 'Todo not found' })
+	}
+})
 
 const message = `Server running: http://localhost:${port}`
 app.listen(port, () => console.log(message))
