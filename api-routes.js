a50f63f8ea3e5b4da77303db1905681917f9ef0d
James <300089902@fvtc.edu>
2024-04-28 15:17:35 -0500

    ObjectId updated

--- api-routes.js
+++ api-routes.js
@@ -5,7 +5,7 @@ const { getCollection } = require('./list-db')
 const dbName = 'List-API'
 const collectionName = 'Todos'
 
-router.get('/', async (request, response) => {
+router.get('/', async (_, response) => {
     const collection = await getCollection(dbName, collectionName)
     const todos = await collection.find({}).toArray()
     response.json(todos)
@@ -20,15 +20,8 @@ router.post('/', async (request, response) => {
 router.put('/:id', async (request, response) => {
     const { id } = request.params
     const { complete } = request.body
-    
-    try {
-        const collection = await getCollection(dbName, collectionName)
-        const result = await collection.updateOne({ _id: id }, { $set: { complete: !complete} })
-    } 
-    catch (error) {
-        console.error("Error updating todo:", error)
-        response.status(500).json({ message: "Error updating todo" })
-    }
+    const collection = await getCollection(dbName, collectionName)
+    const result = await collection.updateOne({ _id: id }, { $set: { complete : !complete } })
 })
 
 module.exports = router
